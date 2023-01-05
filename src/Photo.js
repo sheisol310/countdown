493dd1d8d3870dc2e94310ec0c4bfd6fd7951722
Ching-Kung Lin <71618073+sheisol310@users.noreply.github.com>
2023-01-04 20:37:10 -0500

    Update Photo.js

--- src/Photo.js
+++ src/Photo.js
@@ -25,7 +25,8 @@ import p22 from './img/p22.JPG';
 export default function Photo() {
 
       const [currentIndex, setCurrentIndex] = useState(0);
-      const photos = [p1, p2, p3, p4, p5, p7, p8, p9, p10, p11, p12, p13, p14, p16, p17, p18, p19, p20, p21, p22];
+//       const photos = [p1, p2, p3, p4, p5, p7, p8, p9, p10, p11, p12, p13, p14, p16, p17, p18, p19, p20, p21, p22];
+      const photos = [p1];
     
       
       useEffect(() => {
